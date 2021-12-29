49d12099fda3f7bd8ad324ae6f420bdf7eeb8c06
Willian Prado <willian_prado@hotmail.com>
2021-12-29 02:06:48 -0300

    Add ambiente desenvolvimento

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -114,6 +114,7 @@ class StrangerThings extends React.Component {
         )}` }
       >
         <div className="content strangerfy">
+          <h2>Em desenvolvimento</h2>
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
